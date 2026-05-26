fda0b3828b24ee16ee33c0b2e569bb7ce842b110
drkashifrasheed <drmaharkashifrasheed@gmail.com>
2026-05-26 22:08:50 +0500

    Delete backend directory

--- backend/utils/persistence.js
+++
@@ -1,8 +0,0 @@
-const { isDatabaseConnected } = require('../config/database');
-
-/** Use in-memory auth when MongoDB is not connected (development only). */
-function useMemoryFallback() {
-  return process.env.NODE_ENV !== 'production' && !isDatabaseConnected();
-}
-
-module.exports = { useMemoryFallback };
