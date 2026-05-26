b43a494069edd6a2057de5e3481689eb81acbd12
drkashifrasheed <drmaharkashifrasheed@gmail.com>
2026-05-26 22:09:21 +0500

    Delete scripts directory

--- scripts/kill-ports.ps1
+++
@@ -1,9 +0,0 @@
-# Frees ports before starting dev (Windows)
-$ports = 3000, 5000
-foreach ($port in $ports) {
-  Get-NetTCPConnection -LocalPort $port -State Listen -ErrorAction SilentlyContinue |
-    ForEach-Object {
-      Stop-Process -Id $_.OwningProcess -Force -ErrorAction SilentlyContinue
-    }
-}
-Write-Host "Ports 3000 and 5000 cleared."
